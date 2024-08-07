5769c3869157ab3c925cc19aeabd8289047cd86d
JayanthCS <118911668+JayanthCS@users.noreply.github.com>
2024-08-07 13:42:19 +0530

    Codes

---
+++ JavaScript/data_types.js
@@ -0,0 +1,17 @@
+let age = 20;
+let height = 5.8;
+const PI = 3.142;
+
+
+console.log(Math.round(PI));
+console.log(Math.ceil(PI));
+console.log(Math.floor(PI));
+
+let team="             Royal Challengers Banglore"
+
+console.log(team.includes('Challengerss'))
+console.log(team.length)
+console.log(team.trim().length)
+console.log(team.split(''))
+console.log(team.substr(19,25))
+console.log(team.substring(19,25))
---
+++ JavaScript/intro.js
@@ -0,0 +1,8 @@
+function call_me() {
+    alert('You just invoked call_me function!!!')
+    console.log("You just invoked call_me function!!!")
+ }
+
+ console.log('Hi')
+
+ //document.getElementById('main').innerText="hello jayanth"; 
