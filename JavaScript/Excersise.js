cf2214d4a5c31c124b204d9fbd6f78a9d007b8be
JayanthCS <jayanthcs634xworkz@gmail.com>
2024-08-13 12:46:47 -0700

    Codes

---
+++ JavaScript/Excersise.js
@@ -0,0 +1,9 @@
+let bioData = `Virat Kohli was born on November 5, 1988, in Delhi, India.
+               He grew up in Delhi and was one of the first to train at the West Delhi Cricket Academy,
+               created in 1998. In 2002 he played for the Delhi Under-15 team and was the highest run
+               scorer in the 2003–04 Vijay Merchant Trophy, playing for the Delhi Under-17 team.`
+
+console.log(bioData.length)      
+console.log(bioData.match('in'))  
+console.log(bioData.search('in')) 
+console.log(bioData.includes('in'))        
---
+++ JavaScript/Functions.js
@@ -0,0 +1,63 @@
+function timeNow() {
+    var utc = new Date().toJSON();
+    console.log(utc)
+}
+
+
+function studentGrade(name, marks) {
+    if (marks >= 85) {
+        console.log(`the student ${name} has got distinction`)
+    } else if (marks >= 60) {
+        console.log(`the student ${name} has got First class`)
+    } else if (marks >= 35) {
+        console.log(`the student ${name} has got just pass`)
+    }
+    else {
+        console.log(`the student ${name} has failed`)
+    }
+}
+
+studentGrade('Jayanth', 90)
+studentGrade('Sujay', 70)
+studentGrade('JSujan', 35)
+studentGrade('Sujan', 30)
+
+
+/*function summation() {
+    console.log(arguments)
+}
+summation(1, 2, 3, 4, 5, 6, 7, 23, 45, 67, 45)
+summation(1, 3, 4, 6, 8, 10)
+summation(1, 2)*/
+
+//for(of)
+function summation() {
+    console.log(arguments)
+
+    let sum = 0;
+    for (let v of arguments) {
+        sum = sum + v;
+    }
+    console.log("sum is:", sum)
+}
+summation(1, 2)
+
+
+//Annonimus function
+
+let x = function () {
+    console.log("Hello World!!")
+}
+x()
+console.log(x())
+
+//Self Invoking Function
+
+// (function(){
+//     console.log("Hello!!")
+// })()
+
+//Arrow Function
+
+let cube = (x) => {return x * x * x}
+console.log(cube(3))
