1a1314033dc8caac72fe761967a411a1896ee3af
JayanthCS <jayanthcs634xworkz@gmail.com>
2024-08-14 16:20:39 -0700

    Assignment

--- JavaScript/Assignment.js
+++ JavaScript/Assignment.js
@@ -1,13 +1,20 @@
 //Voting
 
-let age = prompt("Enter your age")
+// let age = prompt("Enter your age")
 
-if (age >= 18) {
-    console.log("You are eligible for voting")
-} else {
-    ageRequiredToVote = 18 - age;
-    console.log(`You still have to wait ${ageRequiredToVote} years for voting`)
-}
+// if (age >= 18) {
+//     console.log("You are eligible for voting")
+// } else {
+//     ageRequiredToVote = 18 - age;
+//     console.log(`You still have to wait ${ageRequiredToVote} years for voting`)
+// }
+
+
+//Reverse an array
+
+let a=['jay','abi','Anni','Karthi']
+
+console.log(a.reverse())
 
 /* What is ECMAScript
 What is the purpose of the array slice method
@@ -81,9 +88,101 @@ to another function so that it can be executed in that other function is called 
 Ans:Typeof operator is used to check the type of variable which is declared.
 
 8,What is the difference between null and undefined
-
+Ans:**undefined**
+ means a variable has been declared but has not yet been assigned a value, 
+ **null**
+is an assignment value, meaning that a variable has been declared and given the value of null
 
 9,What is the difference between an attribute and a property
+Ans:**Attributes**
+They are used to give more details about the element, such as the ID, class, or style. 
+These attributes are only used when the page is first loaded and cannot be changed by JavaScript.
+**Properties**
+are the values that can be accessed and modified through the DOM with JavaScript.
+
+10,How do you remove trailing spaces of a string in javascript
+Ans:The trim() method of String values removes whitespace from both ends of this string and returns a new string,
+without modifying the original string. To return a new string with whitespace trimmed from just one end,
+use trimStart() or trimEnd() .
+
+11,How do you define multiline strings
+Ans:There are three ways to create strings that span multiple lines:
+*By using template literals.
+*By using the + operator – the JavaScript concatenation operator.
+*By using the \ operator – the JavaScript backslash operator and escape character.
+
+12,What would be the result of 1+2+'3'
+Ans:'33'
+
+13,What is an anonymous function, write syntaxt
+Ans:Anonymous function nothing but we can create a function without the function name or
+Anonymous functions are self-invoking or self-executing. This means they are called just after the declaration.
+They are called using brackets() just after declaring the function. 
+If we do not use the invoking brackets after function definition,they will be just defined and not called
+function() {
+    // Function Body
+ }
+or
+(function(){
+  //Function body
+})()
+
+14,What are primitive data types
+Ans:The five primitive data types in JavaScript are String, Number, Boolean, Undefined, and Null.
+
+15,What is nodejs
+Ans:
+Node.js is an open source server environment
+Node.js is free
+Node.js runs on various platforms (Windows, Linux, Unix, Mac OS X, etc.)
+Node.js uses JavaScript on the server
+
+16,What are the different types of loops in javascript
+Ans:
+for - loops through a block of code a number of times
+for/in - loops through the properties of an object
+for/of - loops through the values of an iterable object
+while - loops through a block of code while a specified condition is true
+do/while - also loops through a block of code while a specified condition is true
+
+17,How do you reversing an array
+Ans:We can use reverse() 
+let a=['jay','abi','Anni','Karthi']
+console.log(a.reverse())
+
+18,How do you list all properties of a javascript object
+Ans:The getOwnPropertyNames() method returns all properties. 
+The Object.keys() method returns all enumerable properties.
+If you define object properties without enumerable:false, the two methods will return the same.
+
+19,How do you convert character to ASCII code
+Ans:using JavaScript charCodeAt() method
+
+20,What happens if we add two arrays
+Ans:So, we see now what happens when we add two arrays. In summary, 
+the addition of arrays causes them to be coerced into strings, 
+which does that by joining the elements of the arrays in a comma-separated string and then string-concatenating the joined strings
+let arr1=['jay','abi','Anni','Karthi']
+let arr2=[1,3,6,8]
+console.log(arr1.concat(arr2))
+//output:['jay','abi','Anni','Karthi',1,3,6,8]
+
+21,Is that possible to use expressions in switch cases?
+Ans:This is how it works:
+The switch expression is evaluated once.
+The value of the expression is compared with the values of each case.
+If there is a match, the associated block of code is executed.
+If there is no match, the default code block is executed
+
+22,What are the differences between for...of and for...in statements
 
 */
+
+
+let arr1=['jay','abi','Anni','Karthi']
+let arr2=[1,3,6,8]
+
+console.log(arr1.concat(arr2))
+
+//output:['jay','abi','Anni','Karthi',1,3,6,8]
 
